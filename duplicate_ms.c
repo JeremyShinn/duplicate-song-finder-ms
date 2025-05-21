561837e4889975835f0ec4357eb53b65211f2b85
JeremyShinn <scornedsloth@gmail.com>
2025-05-21 09:13:39 +0000

    Started program

---
+++ duplicate_ms.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int eliminateDuplicates(char* fileName){
+    FILE* data = fopen(fileName, "r");
+
+}
+
+int main(){
+    print(eliminateDuplicates());
+}
