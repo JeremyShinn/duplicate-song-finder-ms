7c6e846b15b34a21d8775f4ae90010f45d828171
JeremyShinn <scornedsloth@gmail.com>
2025-05-22 01:28:34 -0400

    Update duplicate_songs_ms.py

--- duplicate_songs_ms.py
+++ duplicate_songs_ms.py
@@ -1,14 +1,57 @@
 import zmq
+import time
 
-print("Attempting to connect...", end=" ")
-socket = zmq.Context().socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+def remove_duplicates(filepath):
+    duplicates_removed = 0
+    data = []
+
+    with open(filepath, "r") as file:
+        data = file.readlines()
+
+    with open(filepath, "w") as file:
+        i = 1
+        file.write(data[0])
+        num_users = int(data[0].strip("\n"))
+        for user in range(num_users):
+            num_playlists = int(data[i].strip("\n").split(";")[1])
+            file.write(data[i])
+            i += 1
+            for playlist in range(num_playlists):
+                playlist_data = data[i].strip("\n").split(";")
+                songs = []
+                num_songs = int(playlist_data[1])
+                new_num = num_songs
+                i += 1
+                for song in range(num_songs):
+                    if data[i] in songs:
+                        new_num -= 1
+                        duplicates_removed += 1
+                    else:
+                        songs.append(data[i])
+                    i += 1
+                file.write(playlist_data[0] + ";" + str(new_num) + "\n")
+                file.writelines(songs)
+        file.truncate()
+    return duplicates_removed
+
+print("Preparing connection...", end=" ")
+socket = zmq.Context().socket(zmq.REP)
+socket.bind("tcp://*:5555")
 print("Success!\n")
 
-message = "Data.txt"
-print(f"Sending the following file path: {message}")
-socket.send_string(message)
-message = socket.recv()
-print(f"Received the following message: {message.decode()}")
+while True:
+    message = socket.recv()
+    file_path = message.decode()
+    print(f"Message received with the following file path: {file_path}\n")
+    time.sleep(5)
+    print(f"Removing duplicate songs from: {file_path}\n")
+    duplicates = str(remove_duplicates(file_path))
+    time.sleep(5)
+    print(f"{duplicates} duplicates removed!\n")
+    time.sleep(5)
+    print(f"Sending the following message back: {duplicates}")
+    socket.send_string(duplicates)
 
-zmq.Context().destroy()
+
+
+
